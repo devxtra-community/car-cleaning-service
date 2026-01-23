47b438d1e70b3c73391a82aabbc7ae1316cdf37a
athulyarchandra <athulyarchandra24@gmail.com>
2026-01-23 09:20:42 +0530

    feat: created new user table-changed schema

--- backend/src/modules/auth/auth_routes.ts
+++ backend/src/modules/auth/auth_routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { login, registerUser } from './auth_controller';
+import { login, logout, registerUser } from './auth_controller';
 
 const router = Router();
 
 router.post('/register', registerUser);
 router.post('/login', login);
+router.post('/logout', logout);
 
 export default router;
