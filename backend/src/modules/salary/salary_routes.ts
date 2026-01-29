e33aa36ee7b903ade5efcd95fc5741818dc4b910
muhdyasirr <muhdyasir223@gmail.com>
2026-01-29 12:33:17 +0530

    feat(worker): added worker app

--- backend/src/modules/salary/salary_routes.ts
+++ backend/src/modules/salary/salary_routes.ts
@@ -6,10 +6,12 @@ import {
   updateSalary,
   finalizeSalary,
 } from './salary_controller';
+import { protect } from 'src/middlewares/authMiddleware';
+import { allowRoles } from 'src/middlewares/roleMiddleware';
 
 const router = Router();
 
-router.post('/', createSalary);
+router.post('/', protect, allowRoles('super_admin', 'admin'), createSalary);
 router.get('/', getAllSalaries);
 router.get('/me', getMySalary);
 router.put('/:id', updateSalary);
