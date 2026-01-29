e9a680ce69377f0b5ad7f2d2e2088c410106524e
athulyarchandra <athulyarchandra24@gmail.com>
2026-01-29 11:13:57 +0530

    feat: frontend ui for login

---
+++ apps/web/src/services/serverURL.js
@@ -0,0 +1,2 @@
+const SERVER_URL = 'http://localhost:3033';
+export default SERVER_URL;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import cors from 'cors';
+
 import express, { Request, Response } from 'express';
 import { logger } from './config/logger';
 import { connectDatabase } from './database/connectDatabase';
@@ -10,11 +12,21 @@ import authRouter from './modules/auth/auth_routes';
 import vechicleRoutes from './modules/vehicles/vechicleRoutes';
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
+
 const PORT = 3033;
 connectDatabase();
+
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 app.get('/health', (req: Request, res: Response) => {
@@ -26,8 +38,10 @@ app.get('/health', (req: Request, res: Response) => {
     timestamp: new Date().toString(),
   });
 });
+
 app.use('/api/auth', authRouter);
 app.use('/api/vehicle', vechicleRoutes);
+
 app.use(globalErrorHandler);
 
 app.listen(PORT, () => {
