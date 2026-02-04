fbd9f5b0d10a742eb48150f94684c45bbbc61000
muhdyasirr <muhdyasir223@gmail.com>
2026-02-04 10:23:51 +0530

    feat(worker): add home page features

---
+++ backend/src/modules/Worker/workers_controller.ts
@@ -0,0 +1,27 @@
+import { Response } from 'express';
+import { AuthRequest } from '../../middlewares/authMiddleware';
+import { pool } from '../../database/connectDatabase';
+
+export const getWorkerDashboard = async (req: AuthRequest, res: Response) => {
+  try {
+    const workerId = req.user?.userId;
+
+    const worker = await pool.query(`SELECT id, full_name FROM users WHERE id=$1`, [workerId]);
+
+    const jobsDone = await pool.query(
+      `SELECT COUNT(*) FROM tasks WHERE worker_id=$1 AND status='completed'`,
+      [workerId]
+    );
+
+    const wallet = await pool.query(`SELECT balance FROM wallets WHERE worker_id=$1`, [workerId]);
+
+    res.json({
+      name: worker.rows[0].full_name,
+      empId: worker.rows[0].id,
+      jobsDone: Number(jobsDone.rows[0].count),
+      totalRevenue: wallet.rows[0]?.balance || 0, // ⭐ incentive earnings
+    });
+  } catch {
+    res.status(500).json({ message: 'Dashboard failed' });
+  }
+};
--- backend/src/modules/tasks/tasks_routes.ts
+++ backend/src/modules/tasks/tasks_routes.ts
@@ -1,26 +1,17 @@
 import express from 'express';
 import { protect } from '../../middlewares/authMiddleware';
 import { allowRoles } from '../../middlewares/roleMiddleware';
-import { uploadTaskImageToS3 } from '../../middlewares/uploadToS3';
-import { createTaskController } from './tasks_controller';
+import { completeTaskController, createTaskController, GetTaskpending } from './tasks_controller';
 
 const router = express.Router();
-
-router.post(
-  '/',
-  protect,
-  allowRoles('cleaner', 'admin', 'super_admin'),
-  uploadTaskImageToS3.single('car_image'),
-  createTaskController
-);
+router.post('/', protect, allowRoles('worker', 'admin', 'cleaner'), createTaskController);
+router.get('/my', protect, allowRoles('worker', 'cleaner', 'admin', 'super_admin'), GetTaskpending);
 
 router.patch(
-  '/tasks/:id/complete',
+  '/:id/complete',
   protect,
   allowRoles('worker', 'cleaner', 'admin'),
-  createTaskController
+  completeTaskController
 );
-
-router.get('/tasks/my', protect, allowRoles('worker'), createTaskController);
 
 export default router;
